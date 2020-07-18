215f50dc37e3b21cd1836f06840bc00e4415af81
Navdeep Mandal <navdeep.mandal2@gmail.com>
2020-07-18 20:15:56 +0530

    Updates

---
+++ precache-manifest.5c07a9ce9304892c8a19b727d20f6cd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a899dc07bf2c6b1b69fdfa91270e0b1",
+    "url": "/First-React/index.html"
+  },
+  {
+    "revision": "57c17db2c09fe02302b5",
+    "url": "/First-React/static/js/2.e56e9e92.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/First-React/static/js/2.e56e9e92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2b7a40d49bc47419560",
+    "url": "/First-React/static/js/main.bc1387e2.chunk.js"
+  },
+  {
+    "revision": "76754f751598dc15391b",
+    "url": "/First-React/static/js/runtime-main.ba4af3c5.js"
+  }
+]);
